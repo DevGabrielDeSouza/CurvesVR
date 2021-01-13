d7d9b0fb61c705dcb9f3630aca25264275d524cf
GabrielDeSouza <devgabrieldesouza@gmail.com>
2021-01-13 00:07:06 -0300

    Path Creator Project
    Imported Path Creator asset and some examples, including a new one with a script.

---
+++ Assets/Scripts/Follower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PathCreation;
+
+public class Follower : MonoBehaviour
+{
+	[SerializeField]
+	private PathCreator pathCreator;
+	[SerializeField]
+	private float speed = 5;
+	private float distanceTraveled;
+
+	private void Update() {
+		distanceTraveled += speed * Time.deltaTime;
+		transform.position = pathCreator.path.GetPointAtDistance(distanceTraveled);
+	}
+}
